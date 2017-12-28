4d24f9bb80c919651ac689ecc5299371d0d5ac25
Nut Chanyong <nutjaa@msn.com>
2017-12-28 09:26:54 +0700

    Component ++

--- app/pods/components/thai-address/component.js
+++ app/pods/components/thai-address/component.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import fetch from 'fetch';
 const { Component, RSVP, run } = Ember;
 
 const numbers = [
@@ -40,10 +41,109 @@ const users = [
 export default Component.extend({
   numbers,
   users,
+
+  data: [],
+
+  sub_district: '',
+  district: '',
+  province: '',
+  zipcode: '',
+
+  init(){
+    this._super(...arguments);
+    let me = this;
+
+    Ember.$.getJSON( "https://raw.githubusercontent.com/earthchie/jquery.Thailand.js/master/jquery.Thailand.js/database/db.json", function( data ) {
+      me.preprocess(data);
+    });
+  },
+
+  preprocess(data){
+    let lookup = [];
+    let words = [];
+    let expanded = [];
+    let useLookup = false;
+    let t;
+
+    if (data.lookup && data.words) {
+      // compact with dictionary and lookup
+      useLookup = true;
+      lookup = data.lookup.split('|');
+      words = data.words.split('|');
+      data = data.data;
+    }
+
+    t = function (text) {
+      function repl (m) {
+        let ch = m.charCodeAt(0);
+        return words[ch < 97 ? ch - 65 : 26 + ch - 97];
+      }
+      if (!useLookup) {
+        return text;
+      }
+      if (typeof text === 'number') {
+        text = lookup[text];
+      }
+      return text.replace(/[A-Z]/ig, repl);
+    }
+
+    if (!data[0].length) {
+      // non-compacted database
+      return data;
+    }
+    // decompacted database in hierarchical form of:
+    // [["province",[["amphur",[["district",["zip"...]]...]]...]]...]
+    data.map(function (provinces) {
+      let i = 1;
+      if (provinces.length === 3) { // geographic database
+        i = 2;
+      }
+
+      provinces[i].map(function (amphoes) {
+        amphoes[i].map(function (districts) {
+          districts[i] = districts[i] instanceof Array ? districts[i] : [districts[i]];
+          districts[i].map(function (zipcode) {
+            let entry = {
+              district: t(districts[0]),
+              amphoe: t(amphoes[0]),
+              province: t(provinces[0]),
+              zipcode: zipcode
+            }
+            if (i === 2) { // geographic database
+              entry.district_code = districts[1] || false;
+              entry.amphoe_code = amphoes[1] || false;
+              entry.province_code = provinces[1] || false;
+            }
+            expanded.push(entry);
+          });
+        });
+      });
+    });
+    this.set('data',expanded);
+  },
+
+  resolveResultbyField(type, searchStr, maxResult){
+    searchStr = searchStr.toString().trim();
+    let db = this.get('data');
+    if (searchStr === '') {
+      return [];
+    }
+    if (!maxResult) {
+      maxResult = 20;
+    }
+    let possibles = [];
+    try {
+      possibles = db.filter(item => {
+        let regex = new RegExp(searchStr, 'g');
+        return (item[type] || '').toString().match(regex);
+      }).slice(0, maxResult);
+    } catch (e) {
+      return [];
+    }
+    return possibles;
+  },
+
   actions: {
-    search(term) {
-      return numbers.filter((num) => num.indexOf(term) > -1);
-    },
 
     skipShortSearches(term, select) {
       if (term.length <= 1) {
@@ -63,6 +163,23 @@ export default Component.extend({
           }, 600);
         }
       });
+    },
+
+
+    searchBySubDistrict(term){
+      return this.resolveResultbyField('district', term, 20);
+    },
+
+    searchByDistrict(term){
+      return this.resolveResultbyField('amphoe', term, 20);
+    },
+
+    searchByProvince(term){
+      return this.resolveResultbyField('province', term, 20);
+    },
+
+    searchByZipCode(term){
+      return this.resolveResultbyField('zipcode', term, 20);
     }
   }
 });
